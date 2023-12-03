c7240337894672a0bb74613591292308ca2e84d2
uvijay <vs.usha89@gmail.com>
2023-12-03 18:19:45 -0500

    Update CelsiustoF.py

--- CelsiustoF.py
+++ CelsiustoF.py
@@ -1,6 +1,6 @@
-def celsius_to_ferenheit(celsius):
+def celsius_to_fahrenheit(celsius):
     if celsius < -273.15:
         return "That temperature doesn't make sense!"
     else:
         return (celsius * (9/5)) + 32
-print(celsius_to_ferenheit(56))
+print(celsius_to_fahrenheit(56))
